1bb351d90e0f0c6d8fcc7531f91d06052a657ec3
Ishak4875 <ishakjaelani07@gmail.com>
2023-01-31 16:03:25 +0800

    third commit

--- resources/views/v_edit.blade.php
+++ resources/views/v_edit.blade.php
@@ -7,60 +7,58 @@
                 <div class="card-body">
                     <h4 style="text-align: center">Edit Agenda</h4>
                     <form>
-                        <!-- 2 column grid layout with text inputs for the first and last names -->
-                        <div class="row mb-4">
-                          <div class="col">
-                            <div class="form-outline">
-                              <input type="text" id="form6Example1" class="form-control" />
-                              <label class="form-label" for="form6Example1">First name</label>
-                            </div>
-                          </div>
-                          <div class="col">
-                            <div class="form-outline">
-                              <input type="text" id="form6Example2" class="form-control" />
-                              <label class="form-label" for="form6Example2">Last name</label>
-                            </div>
-                          </div>
-                        </div>
-                      
-                        <!-- Text input -->
+                        <h6 class="form-label" for="form6Example3">Nama Agenda</h6>
                         <div class="form-outline mb-4">
-                          <input type="text" id="form6Example3" class="form-control" />
-                          <label class="form-label" for="form6Example3">Company name</label>
+                          <input type="text" style="color: black" placeholder="Masukkan Nama Agenda" value="{{$detail->nama_agenda}}" name="nama_agenda" class="form-control" />
                         </div>
                       
-                        <!-- Text input -->
+                        <h6 class="form-label" for="form6Example4">Tanggal Agenda</h6>
                         <div class="form-outline mb-4">
-                          <input type="text" id="form6Example4" class="form-control" />
-                          <label class="form-label" for="form6Example4">Address</label>
+                          <input type="date" style="color: black" value="{{$detail->tanggal_agenda}}" name="tanggal_agenda" class="form-control" />
                         </div>
-                      
-                        <!-- Email input -->
+
+                        <h6 class="form-label" for="form6Example4">Waktu Agenda</h6>
                         <div class="form-outline mb-4">
-                          <input type="email" id="form6Example5" class="form-control" />
-                          <label class="form-label" for="form6Example5">Email</label>
+                          <input type="time" style="color: black" value="{{$detail->waktu_agenda}}" name="waktu_agenda" class="form-control" />
                         </div>
                       
-                        <!-- Number input -->
+                        <h6 class="form-label" for="form6Example4">Penganggung Jawab</h6>
                         <div class="form-outline mb-4">
-                          <input type="number" id="form6Example6" class="form-control" />
-                          <label class="form-label" for="form6Example6">Phone</label>
+                          <input type="text" value="{{$detail->penanggung_jawab}}" placeholder="Masukkan Nama Penanggung Jawab" style="color: black" name="penanggung_jawab" class="form-control" />
                         </div>
-                      
-                        <!-- Message input -->
+
+                        <h6 class="form-label" for="form6Example4">Via</h6>
                         <div class="form-outline mb-4">
-                          <textarea class="form-control" id="form6Example7" rows="4"></textarea>
-                          <label class="form-label" for="form6Example7">Additional information</label>
+                            <select name="via" style="color: black" class="form-select" aria-label="Default select example">
+                                <option <?php if(($detail->via) == 'Online') echo 'selected'?> value="Online">Online</option>
+                                <option <?php if(($detail->via) == 'Offline') echo 'selected'?> value="Offline">Offline</option>
+                            </select>
                         </div>
-                      
-                        <!-- Checkbox -->
-                        <div class="form-check d-flex justify-content-center mb-4">
-                          <input class="form-check-input me-2" type="checkbox" value="" id="form6Example8" checked />
-                          <label class="form-check-label" for="form6Example8"> Create an account? </label>
+
+                        <h6 class="form-label" for="form6Example4">Ruangan</h6>
+                        <div class="form-outline mb-4">
+                          <input type="text" value="{{$detail->ruang}}" placeholder="Masukkan Ruangan" style="color: black" name="ruangan" class="form-control" />
+                        </div>
+
+                        <h6 class="form-label" for="form6Example4">Mengundang Pak Kanwil</h6>
+                        <div class="form-outline mb-4">
+                            <select name="status" style="color: black" class="form-select" aria-label="Default select example">
+                                <option <?php if(($detail->mengundang_pak_kanwil) == 'Mengundang Pak Kanwil') echo 'selected'?> value="Mengundang Pak Kanwil">Mengundang Pak Kanwil</option>
+                                <option <?php if(($detail->mengundang_pak_kanwil) == 'Belum Mengundang Pak Kanwil') echo 'selected'?> value="Belum Mengundang Pak Kanwil">Belum Mengundang Pak Kanwil</option>
+                            </select>
+                        </div>
+
+                        <h6 class="form-label" for="form6Example4">Status</h6>
+                        <div class="form-outline mb-4">
+                            <select name="status" style="color: black" class="form-select" aria-label="Default select example">
+                                <option <?php if(($detail->status) == 'Belum Berlangsung') echo 'selected'?> value="Belum Berlangsung">Belum Berlangsung</option>
+                                <option <?php if(($detail->status) == 'Sedang Berlangsung') echo 'selected'?> value="Sedang Berlangsung">Sedang Berlangsung</option>
+                                <option <?php if(($detail->status) == 'Sudah Berlangsung') echo 'selected'?> value="Sudah Berlangsung">Sudah Berlangsung</option>
+                            </select>
                         </div>
                       
                         <!-- Submit button -->
-                        <button type="submit" class="btn btn-primary btn-block mb-4">Place order</button>
+                        <button type="submit" class="btn btn-primary btn-block mb-4">Submit</button>
                     </form>
                 </div>
             </div>
